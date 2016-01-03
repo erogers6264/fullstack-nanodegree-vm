3029eb0e70387e65973e938505e7aa05deceec44
Ethan Rogers <erogers6264@gmail.com>
2016-01-03 13:48:21 -0700

    Remove unnecessary tables for submission

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,19 +5,12 @@ CREATE DATABASE tournament;
 -- Connect to the database
 \c tournament;
 
--- Create the tournaments table
-CREATE TABLE tournaments(
-	tournament_id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL
-);
-
 -- Create the players table
 CREATE TABLE players(
 	player_id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL,
 	wins INTEGER DEFAULT 0,
 	losses INTEGER DEFAULT 0,
-	tournament INTEGER REFERENCES tournaments (tournament_id),
 	has_bye	BOOLEAN DEFAULT FALSE
 );
 
@@ -26,7 +19,6 @@ CREATE TABLE matches(
 	match_id SERIAL PRIMARY KEY,
 	winner INTEGER REFERENCES players (player_id),
 	loser INTEGER REFERENCES players (player_id),
-	tournament INTEGER REFERENCES tournaments (tournament_id)
 );
 
 -- Create a view of players ordered by wins
